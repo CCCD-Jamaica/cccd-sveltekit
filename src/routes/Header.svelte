cb6e82a01646e71fe285613f5fd4cf828824076f
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-09 15:17:55 -0700

    Updated Header and Footer

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,7 @@
 <script>
   import { slide, fade } from 'svelte/transition';
   import { quintOut } from 'svelte/easing';
+  import Icon from '@iconify/svelte';
   let isMenuOpen = false;
 
   function toggleMenu() {
@@ -8,36 +9,47 @@
   }
 </script>
 
-<header class="bg-gold-50">
+<header class="bg-gold-50 pb-4 border-b-2 border-red-800">
   <div class="bg-red-800 mx-auto flex max-w-7xl items-center justify-center xl:rounded-b-lg" aria-label="Global">
     <span class="text-white uppercase font-bold p-2">Reach, Teach, and Nurture the Deaf</span>
   </div>
-  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-    <div class="flex lg:flex-1">
-      <a href="#" class="-m-1.5 p-1.5">
-        <span class="sr-only">Your Company</span>
-        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+  <div class="hidden lg:flex justify-center py-4">
+    <a href="/" class="-m-1.5 p-1.5">
+      <span class="sr-only"><abbr title="Caribbean Christian Center for the Deaf">CCCD</abbr> Jamaica</span>
+      <img class="h-16 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+    </a>
+  </div>
+  <nav class="mx-auto flex max-w-7xl items-center justify-between lg:py-4 lg:px-8" aria-label="Global">
+    <div class="hidden lg:flex sm:gap-x-2 md:gap-x-6 lg:gap-x-12">
+      <a href="/" class="hover:text-red-800">
+        <Icon icon="material-symbols:church" width="24" height="24" />
       </a>
-    </div>
-    <div class="flex lg:hidden">
-      <button on:click={toggleMenu} type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-        <span class="sr-only">Open main menu</span>
-        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-      </button>
-    </div>
-    <div class="hidden lg:flex lg:gap-x-12">
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+      <a href="about" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">About</a>
+      <a href="education" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Education</a>
+      <a href="stories" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Stories</a>
+      <a href="get-involved" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Get Involved</a>
+      <a href="fundraising" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Fundraising</a>
+      <a href="resources" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Resources</a>
+      <a href="contact" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Contact</a>
+      <!-- Add Search and icon later -->
     </div>
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      <a href="#" class="text-md font-semibold leading-6 text-gray-900 hover:text-red-800">Log in <span aria-hidden="true">&rarr;</span></a>
     </div>
   </nav>
   <!-- Mobile menu, show/hide based on menu open state. -->
+  <div class="lg:hidden flex items-center justify-between p-4">
+    <a href="#" class="-m-1.5 p-1.5">
+      <span class="sr-only"><abbr title="Caribbean Christian Center for the Deaf">CCCD</abbr> Jamaica</span>
+      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+    </a>
+    <button on:click={toggleMenu} type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+      <span class="sr-only">Open main menu</span>
+      <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
+    </button>
+  </div>
   {#if isMenuOpen}
     <div class="lg:hidden" role="dialog" aria-modal="true" transition:fade={{ duration: 300 }}>
       <!-- Background backdrop, show/hide based on slide-over state. -->
@@ -45,7 +57,7 @@
       <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10" transition:slide={{ duration: 300, easing: quintOut }}>
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
+            <span class="sr-only"><abbr title="">CCCD</abbr> Jamaica</span>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
           </a>
           <button on:click={toggleMenu} type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
@@ -58,10 +70,17 @@
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+              <a href="/" class="-mx-3  rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 flex items-center">
+                <Icon icon="material-symbols:church" width="24" height="24" class="mr-2" />
+              </a>
+              <a href="about" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>
+              <a href="education" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Education</a>
+              <a href="stories" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Stories</a>
+              <a href="get-involved" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Get Involved</a>
+              <a href="fundraising" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Fundraising</a>
+              <a href="resources" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Resources</a>
+              <a href="contact" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact</a>
+              <!-- Add Search and icon later -->
             </div>
             <div class="py-6">
               <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
