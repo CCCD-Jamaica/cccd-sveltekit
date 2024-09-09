e11d5f394df82ca33179049b00b9dd85192cb959
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-09 13:28:33 -0700

    Added Hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,24 @@
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Caribben Christian Center for the Deaf in Jamaica" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-	<p class="text-red-500 pl-40">
-		Hello world
-	</p>
+<section class="flex flex-col md:flex-row">
+	<div class="w-1/2">
+	<!-- First column content -->
+	</div>
+	<div class="w-1/2 grid grid-cols-2 gap-4">
+		<div>
+			<!-- Second column, first grid item -->test
+		</div>
+		<div>
+			<!-- Second column, second grid item -->test 2
+		</div>
+		<div>
+			<!-- Second column, third grid item -->test 3
+		</div>
+		<div>
+			<!-- Second column, fourth grid item -->test 4
+		</div>
+	</div>
 </section>
