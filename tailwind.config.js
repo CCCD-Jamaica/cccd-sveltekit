ac4c4f539b25ed048497cd9b76ea9539070eaa47
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-09 09:19:38 -0700

    Added Tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import formsPlugin from '@tailwindcss/forms';
+import typographyPlugin from '@tailwindcss/typography';
+import aspectRatioPlugin from '@tailwindcss/aspect-ratio';
+import containerQueriesPlugin from '@tailwindcss/container-queries';
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+    corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    formsPlugin,
+    typographyPlugin,
+    aspectRatioPlugin,
+    containerQueriesPlugin,
+  ],
 }
