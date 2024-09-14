034bab88960e68be4d20681eb77eedf5923e0b65
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-14 14:40:11 -0700

    Edited Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
-	import BackToTop from "../lib/BackToTop.svelte";
+	import BackToTop from "$components/BackToTop.svelte";
 	import '../app.css';
 </script>
 
