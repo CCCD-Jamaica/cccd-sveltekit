4c7c545378a878e80e20ff613ee8dc8a65709ae1
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-15 15:40:50 -0700

    Updated homepage

--- src/components/Support.svelte
+++ src/components/Support.svelte
@@ -1,11 +1,19 @@
-<section class="bg-red-800 text-white py-12">
-	<div class="container mx-auto px-4">
+<section class="bg-gold-300 text-white py-12 relative border-t border-b border-blue-800">
+	<div class="absolute inset-0 bg-cover bg-center opacity-30" style="background-image: url('https://res.cloudinary.com/shinkirin/image/upload/v1726438628/cccd_wordpress/supportimage.webp');"></div>
+	<div class="container mx-auto px-4 relative z-10">
 		<h2 class="text-3xl font-bold mb-4 text-center">Support Our Mission</h2>
-		<p class="text-lg mb-6 text-center">
-			Join us in making a difference in the lives of the Deaf community in Jamaica. Your support through donations or volunteering can help us continue to Reach, Teach, and Nurture. Together, we can create a brighter future.
-		</p>
-		<div class="flex justify-center">
-			<a href="/get-involved" class="bg-white text-red-800 font-bold py-3 px-6 rounded-lg hover:bg-red-100 transition duration-300">
+		<div class="flex flex-col items-center">
+			<div class="mb-6">
+				<img class="rounded-lg shadow-lg w-1/3 h-auto mx-auto border-2 border-white" src="https://res.cloudinary.com/shinkirin/image/upload/v1726438628/cccd_wordpress/supportimage.webp" alt="Supporting the Deaf community">
+			</div>
+			<div class="max-w-2xl text-center">
+				<p class="text-lg">
+					Join us in making a difference in the lives of the Deaf community in Jamaica. Your support through donations or volunteering can help us continue to Reach, Teach, and Nurture. Together, we can create a brighter future.
+				</p>
+			</div>
+		</div>
+		<div class="flex justify-center pt-4">
+			<a href="/get-involved" class="bg-white text-red-800 font-bold py-3 px-6 rounded-lg hover:bg-red-100 transition duration-300 uppercase">
 				Get Involved
 			</a>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -191,80 +191,82 @@
 				<p class="mt-6 text-lg leading-8 text-black">Your support can make a significant difference in the lives of deaf students in Jamaica. Whether through volunteering, donations, or becoming a missionary, every contribution helps us reach, teach, and nurture the deaf community.</p>
 				<img src="https://res.cloudinary.com/shinkirin/image/upload/v1726434833/cccd/backtoschoolcccd.webp" alt="Back to school wih four CCCD Jamaica Deaf Student" class="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]">
 			</div>
-			<div class="w-full lg:max-w-xl lg:flex-auto">
-				<h3 class="sr-only">Ways to Help</h3>
-				<ul class="-my-8 divide-y divide-gold-300">
-				<li class="py-8">
-					<dl class="relative flex flex-wrap gap-x-3">
-					<dt class="sr-only">Role</dt>
-					<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
-						<a href="/get-involved#volunteer">
-						Volunteer Opportunities
-						<span class="absolute inset-0" aria-hidden="true"></span>
-						</a>
-					</dd>
-					<dt class="sr-only">Description</dt>
-					<dd class="mt-2 w-full flex-none text-base leading-7 text-gray-600">Join us in Jamaica to help with teaching, mentoring, or supporting our various programs. Your skills and time can make a lasting impact on our students' lives.</dd>
-					<dt class="sr-only">Duration</dt>
-					<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">Short-term and long-term options available</dd>
-					<dt class="sr-only">Location</dt>
-					<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
-						<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
-						<circle cx="1" cy="1" r="1" />
-						</svg>
-						Various locations in Jamaica
-					</dd>
-					</dl>
-				</li>
-				<li class="py-8">
-					<dl class="relative flex flex-wrap gap-x-3">
-					<dt class="sr-only">Role</dt>
-					<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
-						<a href="/get-involved#missionary">
-						Missionary Opportunities
-						<span class="absolute inset-0" aria-hidden="true"></span>
-						</a>
-					</dd>
-					<dt class="sr-only">Description</dt>
-					<dd class="mt-2 w-full flex-none text-base leading-7 text-gray-600">Serve as a full-time missionary, dedicating your skills and passion to our mission of reaching and teaching the deaf in Jamaica. Experience personal growth while making a difference.</dd>
-					<dt class="sr-only">Commitment</dt>
-					<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">Long-term commitment required</dd>
-					<dt class="sr-only">Location</dt>
-					<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
-						<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
-						<circle cx="1" cy="1" r="1" />
-						</svg>
-						Jamaica
-					</dd>
-					</dl>
-				</li>
-				<li class="py-8">
-					<dl class="relative flex flex-wrap gap-x-3">
-					<dt class="sr-only">Role</dt>
-					<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
-						<a href="/get-involved#donate">
-						Make a Donation
-						<span class="absolute inset-0" aria-hidden="true"></span>
-						</a>
-					</dd>
-					<dt class="sr-only">Description</dt>
-					<dd class="mt-2 w-full flex-none text-base leading-7 text-gray-600">Your financial support helps us provide education, resources, and opportunities for deaf students in Jamaica. Every contribution, big or small, makes a difference in their lives.</dd>
-					<dt class="sr-only">Options</dt>
-					<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">One-time or recurring donations accepted</dd>
-					<dt class="sr-only">Method</dt>
-					<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
-						<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
-						<circle cx="1" cy="1" r="1" />
-						</svg>
-						Secure online payment options available
-					</dd>
-					</dl>
-				</li>
-				</ul>
-				<div class="mt-8 flex border-t border-gold-300 pt-8">
-				<a href="/get-involved" class="text-sm font-semibold leading-6 text-blue-800 hover:text-red-800">Explore all ways to help <span aria-hidden="true">&rarr;</span></a>
+			<div class="w-full lg:max-w-xl lg:flex-auto relative">
+				<div class="absolute inset-0 bg-no-repeat bg-center opacity-20" style="background-image: url('https://res.cloudinary.com/shinkirin/image/upload/v1722901294/cccd_wordpress/cropped-cccdLogo-drupal.webp'); background-size: 75%; background-position: center;"></div>
+				<div class="relative z-10">
+					<h3 class="sr-only">Ways to Help</h3>
+					<ul class="-my-8 divide-y divide-gold-300">
+					<li class="py-8">
+						<dl class="relative flex flex-wrap gap-x-3">
+						<dt class="sr-only">Role</dt>
+						<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
+							<a href="/get-involved#volunteer">
+							Volunteer Opportunities
+							<span class="absolute inset-0" aria-hidden="true"></span>
+							</a>
+						</dd>
+						<dt class="sr-only">Description</dt>
+						<dd class="mt-2 w-full flex-none text-base leading-7 text-black">Join us in Jamaica to help with teaching, mentoring, or supporting our various programs. Your skills and time can make a lasting impact on our students' lives.</dd>
+						<dt class="sr-only">Duration</dt>
+						<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">Short-term and long-term options available</dd>
+						<dt class="sr-only">Location</dt>
+						<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-black">
+							<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
+							<circle cx="1" cy="1" r="1" />
+							</svg>
+							Various locations in Jamaica
+						</dd>
+						</dl>
+					</li>
+					<li class="py-8">
+						<dl class="relative flex flex-wrap gap-x-3">
+						<dt class="sr-only">Role</dt>
+						<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
+							<a href="/get-involved#missionary">
+							Missionary Opportunities
+							<span class="absolute inset-0" aria-hidden="true"></span>
+							</a>
+						</dd>
+						<dt class="sr-only">Description</dt>
+						<dd class="mt-2 w-full flex-none text-base leading-7 text-black">Serve as a full-time missionary, dedicating your skills and passion to our mission of reaching and teaching the deaf in Jamaica. Experience personal growth while making a difference.</dd>
+						<dt class="sr-only">Commitment</dt>
+						<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">Long-term commitment required</dd>
+						<dt class="sr-only">Location</dt>
+						<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-black">
+							<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
+							<circle cx="1" cy="1" r="1" />
+							</svg>
+							Jamaica
+						</dd>
+						</dl>
+					</li>
+					<li class="py-8">
+						<dl class="relative flex flex-wrap gap-x-3">
+						<dt class="sr-only">Role</dt>
+						<dd class="w-full flex-none text-lg font-semibold tracking-tight text-red-900">
+							<a href="/get-involved#donate">
+							Make a Donation
+							<span class="absolute inset-0" aria-hidden="true"></span>
+							</a>
+						</dd>
+						<dt class="sr-only">Description</dt>
+						<dd class="mt-2 w-full flex-none text-base leading-7 text-black">Your financial support helps us provide education, resources, and opportunities for deaf students in Jamaica. Every contribution, big or small, makes a difference in their lives.</dd>
+						<dt class="sr-only">Options</dt>
+						<dd class="mt-4 text-base font-semibold leading-7 text-blue-800">One-time or recurring donations accepted</dd>
+						<dt class="sr-only">Method</dt>
+						<dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-">
+							<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gold-300" aria-hidden="true">
+							<circle cx="1" cy="1" r="1" />
+							</svg>
+							Secure online payment options available
+						</dd>
+						</dl>
+					</li>
+					</ul>
+					<div class="mt-8 flex border-t border-gold-300 pt-8">
+					<a href="/get-involved" class="text-sm font-semibold leading-6 text-blue-800 hover:text-red-800">Explore all ways to help <span aria-hidden="true">&rarr;</span></a>
+					</div>
 				</div>
-			</div>
 			</div>
 		</div>
 		</div>
@@ -385,17 +387,25 @@
 		</div>
 		</div>
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
-		<div class="bg-gray-100 p-6 rounded-lg">
-			<h3 class="text-xl font-semibold mb-2">Deaf Village</h3>
-			<p>Address: 303 Deaf Village Rd, St. Elizabeth, Jamaica</p>
-			<p>Phone: (876) 567-8901</p>
+		<div class="bg-gray-100 p-6 rounded-lg relative overflow-hidden">
+			<div class="absolute inset-0 bg-cover bg-center opacity-20" style="background-image: url('https://res.cloudinary.com/shinkirin/image/upload/c_scale,o_100,w_850/v1726439904/cccd_wordpress/20240725_104312.webp');"></div>
+			<div class="relative z-10">
+				<h3 class="text-xl font-semibold mb-2">Deaf Village</h3>
+				<p>Address: 303 Deaf Village Rd, St. Elizabeth, Jamaica</p>
+				<p>Phone: (876) 567-8901</p>
+			</div>
 		</div>
 		<div class="bg-gray-100 p-6 rounded-lg">
-			<img 
-			src="https://res.cloudinary.com/your-cloud-name/image/upload/v1234567890/deaf-village.jpg" 
-			alt="Deaf Village" 
-			class="w-full h-auto rounded-lg"
-			/>
+			<div class="relative">
+				<img 
+					src="https://res.cloudinary.com/shinkirin/image/upload/c_scale,w_1400/v1726439540/cccd_wordpress/20240725_105731.webp" 
+					alt="Jamaican Deaf Village" 
+					class="h-full w-auto rounded-lg"
+				/>
+				<span class="absolute bottom-2 right-2 bg-black bg-opacity-50 text-white px-2 py-1 text-sm rounded">
+					Jamaican Deaf Village
+				</span>
+			</div>
 		</div>
 		</div>
 		<div class="mt-12">
