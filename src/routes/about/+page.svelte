82702e2e7c73bcde2dcab8d3522a62602388bb64
Kevin Miller, Jr <kevinmillerjr@gmail.com>
2024-09-22 16:24:51 -0700

    First rounds of accessibility checks

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -50,6 +50,10 @@
     }}
 />
 
+<!-- Accessibility Checks
+	1. silktide: WCAG 2.2 AA Compliance on 2024-09-22.
+-->
+
 <section class="bg-white py-12">
 	<div class="container mx-auto px-4">
 		<h2 class="text-3xl font-bold text-center mb-8 text-red-900">Our Mission and Vision</h2>
